6e283eb1c4bbdac80556fa3d74cff272ad9f9233
speedstershubham <shubhamchowksay10@gmail.com>
2020-06-26 12:23:38 +0530

    first commit

---
+++ build/precache-manifest.26c97a3cc17f78460c5bab6bbd24f69a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506ab3a7dd751e61df72169ebf915013",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bd5733bcc131c6758d2",
+    "url": "/static/js/2.83328e24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.83328e24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7895133d1947559a394b",
+    "url": "/static/js/main.a575b6ea.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "1bb92d051cd0493ea06aed5b2575a4eb",
+    "url": "/static/media/avengers.1bb92d05.jpg"
+  }
+]);
