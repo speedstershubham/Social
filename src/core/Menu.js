e155645ac99f02b1f45780d623bf42f769795d3b
speedstershubham <shubhamchowksay10@gmail.com>
2020-07-06 12:08:58 +0530

    first commit

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -11,12 +11,12 @@ else return {color:"#ffffff"};
 
 const Menu = ({history}) => (
     <div>
-        <ul className="nav nav-tabs  bg-primary ">
+        <ul className="nav nav-tabs bg-primary navbar-right ">
     <li className="nav-item"> <Link className="nav-link" style={isActive(history,"/")} to="/" > Home</Link></li>
     <li className="nav-item"> <Link className="nav-link" style={isActive(history,"/users")} to="/users" > Friends</Link></li>
   {!isAuthenticated() && (
       <>
-       <li className="nav-item navitem-right">
+       <li className="nav-item">
        <Link className="nav-link" style={isActive(history,"/signin")} to="/signin"> SignIn</Link></li>
        <li className="nav-item"><Link className="nav-link" style={isActive(history,"/signup")} to="/signup"> SignUp</Link></li>
        </>
@@ -25,21 +25,21 @@ const Menu = ({history}) => (
 {isAuthenticated() && (
     <>
  
- <li className="nav-item navbar-right"><a className="nav-link" >
+ <li className="nav-item"><a className="nav-link" >
      <Link to={`/findpeople`} style={isActive(history,`/findpeople`)}>
 Find People</Link></a></li>
 
-<li className="nav-item navbar-right"><a className="nav-link" >
+<li className="nav-item"><a className="nav-link" >
      <Link to={`/post/create`} style={isActive(history,`/post/create`)}>
 Create Post</Link></a></li>
 
 
 
- <li className="nav-item navbar-right"><a className="nav-link" >
+ <li className="nav-item"><a className="nav-link" >
      <Link to={`/user/${isAuthenticated().user._id}`} style={isActive(history,`/user/${isAuthenticated().user._id}`)}>
 { `${isAuthenticated().user.name}'s Profile `} </Link></a></li>
 
-<li className="nav-item navbar-right"><span className="nav-link" style={isActive(history,"/signup"),
+<li className="nav-item"><span className="nav-link" style={isActive(history,"/signup"),
  {cursor:"pointer",color:"#fff"}}
  onClick={() => signout(() => history.push('/'))}
  > SignOut</span></li>
@@ -48,8 +48,9 @@ Create Post</Link></a></li>
 )}
 {isAuthenticated() &&  isAuthenticated().user.role === "admin" && (
     <>
-    <li className="nav-item navbar-right">
+    <li className="nav-item">
        <Link
+            to={`/admin`}
             style={isActive(history, `/admin`)}
             className="nav-link"
         > Admin</Link>
